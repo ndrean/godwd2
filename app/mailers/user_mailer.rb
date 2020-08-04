2d1dd64d0725a83929973492c841ee822ef78862
neven <nevendrean@yahoo.fr>
2020-08-05 00:10:07 +0200

    images readme

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,13 @@ class UserMailer < ApplicationMailer
     def register(user_email, user_confirmation_token)
         @user_email = user_email
         @user_confirmation_token = user_confirmation_token
-        mail(to: @user_email,  subject: "Confirm registration")
+        mail(to: @user_email,  subject: "Confirm your registration")
     end
 
     def accept(current_user_email, owner_email, itinary_id)
         @current = current_user_email
         @owner = owner_email
         @itinary = Itinary.find(itinary_id)
-        mail(to: @current, subject: 'Accept demand' )
+        mail(to: @current, subject: 'Demand Accepted!' )
     end
 end
